256faffeb0efcb0c62adea5b101259277f156a0f
gdm-1718-miradegr <miradegr@student.arteveldehs.be>
2018-03-29 14:09:40 +0200

    week 7 start oef 2

--- week7/Oefening 2 Foodcourt/script.js
+++ week7/Oefening 2 Foodcourt/script.js
@@ -15,15 +15,35 @@ const getJSON = function(url, callback) {
 };
 
 // get json data
-getJSON('http://api.icndb.com/jokes/random', function(error, data) {
-    
+getJSON('https://evelienrutsaert.github.io/recourses/recipes.json', function(error, data) {
     // show error
-    if(error) {       
+    if(error) {  
     // do something here
     return false;
     }
     console.log(data);
-    if(data) {
-    console.log(data)
-    }
-    });
+
+// recept = function(){
+
+// uit html
+  let titel= document.getElementsByClassName('card-title');
+  let afb= document.getElementsByClassName('card-img-top');
+  let ingr= document.getElementsByClassName('card-ingredients');
+  // directions
+
+// uit json
+  let naam= data.name;
+  let afbeelding= data.image;
+  for (i=0; i<4; i++) {
+    let ingredienten; // = data.ingredients[0];
+    ingredienten += data.ingredients[i];
+    console.log(ingredienten);
+}
+  
+  titel[0].innerHTML =data[0].name; 
+  afb[0].src=data[0].image; 
+  ingr[0].innerHTML=data[0].ingredienten;
+// };
+// recept()
+
+});
