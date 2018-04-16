513debfe25f90e5aef72efd2101a202ffd3c28ff
gdm-1718-miradegr <miradegr@student.arteveldehs.be>
2018-04-16 09:04:05 +0200

    dashboard

--- extra basis/oefening5priemgetal/script.js
+++ extra basis/oefening5priemgetal/script.js
@@ -2,20 +2,63 @@ let getal= parseInt(prompt('Geef een getal in'));
 
 
 let primeNumber=function(getal){
+
+    console.log('Start primeNumber');
+    console.log('getal:' +getal);
+
     let alleRest;
     let rest;
     
-    for(i=2; i=getal.length; i++){
+    for(i=2; i<getal; i++){
         rest=getal%i;
         alleRest += rest;
+        console.log('alleRest:' + alleRest);
+        console.log(' i : ' + i);
     }
-    if(getal>1 && rest==0){
+    if(alleRest != 0){
+       document.write('Dit is GEEN priemgetal') 
+    }
+    else{
+        document.write('Dit is een priemgetal')
+    }    
+       
+    console.log('End primeNumber');
+};
+
+
+let primeNumber2=function(getal){
+
+    console.log('Start primeNumber 2');
+    console.log('getal:' +getal);
+
+    let alleRest;
+    let rest;
+    
+
+    let isPriem = true;
+    let continueLoop = true;
+    
+    console.log('isPriem:' +isPriem);
+
+    for(i=2; i<getal && continueLoop ; i++){
+        rest=getal%i;
+        console.log(' i : ' + i + ', rest:' + rest);
+        if (rest == 0) {
+            console.log('Geen priem, stop looping');
+            isPriem = false;
+            continueLoop = false;
+        }
+    }
+    if(isPriem==true) {
         document.write('Dit is een priemgetal')
     }
     else{
-        document.write('Dit is GEEN priemgetal')
+        document.write('Dit is GEEN priemgetal') 
+        
     }    
        
+    console.log('End primeNumber 2');
 };
 
-primeNumber(getal);
+
+primeNumber2(getal);
--- personal dashboard/script.js
+++ personal dashboard/script.js
@@ -1,8 +1,138 @@
-var url = 'https://newsapi.org/v2/top-headlines?' +
-          'country=us&' +
-          'apiKey=KEY';
-var req = new Request(url);
+// WEER ESSENE
+
+// functie expressie om JSON-request via url uit te voeren
+const getJSON = function(url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.responseType = 'json';
+    xhr.onload = function() {
+      var status = xhr.status;
+      if (status === 200) {
+        callback(null, xhr.response);
+      } else {
+        callback(status, xhr.response);
+      }
+    };
+    xhr.send();
+};
+
+
+// get json data
+getJSON('http://api.openweathermap.org/data/2.5/weather?APPID=548140ccdc7dd4f8b901f91fde69fdc2&q=Essene', function(error, data) {
+    // show error
+    if(error) {  
+        // do something here
+        return false;
+    }
+    else{
+        console.log(data);
+        let divEssene = document.getElementById('weatherEs');
+        
+        divEssene.innerHTML=
+            'Algemeen: ' +data.weather[0].description + '<br>' +
+            'Vochtigheid: ' + data.main.humidity + ' %'+'<br>' + 
+            'Luchtdruk: ' + data.main.pressure + ' hPa' +'<br>' + 
+            'Temperatuur: '+ (parseFloat(data.main.temp)-273.15) +' °C' + '<br>'+ 
+            'Maximum temperatuur: '+ (parseFloat(data.main.temp_max)-273.15) +' °C'+'<br>'+
+            'Minimum temperatuur: '+ (parseFloat(data.main.temp_min)-273.15) +' °C'  ;
+
+        let icon = data.weather[0].icon;
+        console.log(icon);
+        let iconurl = 'http://openweathermap.org/img/w/'+icon+".png";
+
+        let image = document.createElement("img");
+        image.setAttribute("src", iconurl);
+
+        let first = document.getElementById('1');
+        first.appendChild(image);
+     }
+ });
+
+// WEER MARIAKERKE
+
+getJSON('http://api.openweathermap.org/data/2.5/weather?APPID=548140ccdc7dd4f8b901f91fde69fdc2&q=Mariakerke', function(error, data) {
+    // show error
+    if(error) {  
+        // do something here
+        return false;
+    }
+    else{
+        console.log(data);
+        let divMariakerke = document.getElementById('weatherMa');
+        
+        divMariakerke.innerHTML=
+            'Algemeen: ' +data.weather[0].description + '<br>' +
+            'Vochtigheid: ' + data.main.humidity + ' %'+ '<br>' + 
+            'Luchtdruk: ' + data.main.pressure + ' hPa' + '<br>' + 
+            'Temperatuur: '+ (parseFloat(data.main.temp)-273.15) +' °C' + '<br>'+ 
+            'Maximum temperatuur: '+ (parseFloat(data.main.temp_max)-273.15) +' °C'+'<br>'+
+            'Minimum temperatuur: '+ (parseFloat(data.main.temp_min)-273.15) +' °C'  ;
+
+        let icon2 = data.weather[0].icon;
+        console.log(icon2);
+        let iconurl2 = 'http://openweathermap.org/img/w/'+icon2+".png";
+
+        let image2 = document.createElement("img");
+        image2.setAttribute("src", iconurl2);
+
+        let second = document.getElementById('2');
+        second.appendChild(image2);
+     }
+ });
+
+// NIEUWS
+
+const url = 'https://newsapi.org/v2/top-headlines?' +
+          'sources=national-geographic&' +
+          'apiKey=2a01932539f0474ab84d2129b36cc932';
+const req = new Request(url);
 fetch(req)
     .then(function(response) {
-        console.log(response.json());
-    })
+        console.log(response.json());        
+    });
+
+
+// get json data
+getJSON('https://newsapi.org/v2/top-headlines?sources=national-geographic&apiKey=2a01932539f0474ab84d2129b36cc932', function(error, data) {
+    // show error
+    if(error) {  
+        // do something here
+        return false;
+    }
+
+    console.log(data);
+    // for(i=0; i<data.articles.length;i++){
+    //     console.log([i]);
+    // }
+    
+    for(i=0; i<5; i++){
+        
+        let createArticle = function(i){
+
+            // AFBEELDINGEN WERKEN NOG NIET
+
+            // let foto = data.articles[i].urlToImage;
+            // let fotoUrl = '';
+    
+            // let image = document.createElement("img");
+            // image.setAttribute("src", fotoUrl);
+    
+            let heading ='<strong>' +data.articles[i].title +'</strong>';
+        
+            let artikel = heading + '<br>' + data.articles[i].description + '<br>'+data.articles[0].url;
+            console.log('artikel: '+ artikel);
+            let inHTML = document.getElementById('nieuws');
+            let p = document.createElement('p');
+            p.setAttribute('class', 'paragraaf');
+    
+            p.innerHTML = artikel;
+        
+            inHTML.appendChild(p);
+            // inHTML.appendChild(image);
+        };
+    
+    createArticle(i)  
+    }
+
+});
+
